3620f919e3d7d3906f68984a7428ca7637403e4d
Semphris <semphris@semphris.com>
2022-07-03 14:37:37 -0400

    Basic test suite with tests for Vector class
    This test suite reduces the risk that a bug is introduced.

---
+++ tests/tests.cpp
@@ -0,0 +1,180 @@
+//  SuperTux Meltdown - Semphris' take on the popular Linux platformer
+//  Copyright (C) 2022 Semphris <semphris@semphris.com>
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#include "tests.hpp"
+
+#include <chrono>
+#include <iostream>
+
+static std::string str_time(float t)
+{
+  std::stringstream oss;
+  oss.precision(3);
+
+  if (t < 1e-6f)
+  {
+    oss << (t * 1e9f) << " ns";
+  }
+  else if (t < 1e-3f)
+  {
+    oss << (t * 1e6f) << " us";
+  }
+  else if (t < 1.0f)
+  {
+    oss << (t * 1e3f) << " ms";
+  }
+  else
+  {
+    oss << t << " s";
+  }
+
+  return oss.str();
+}
+
+std::vector<std::pair<std::string, void(*)()>>&
+g_tests(const std::string& name, void (*func)())
+{
+  static std::vector<std::pair<std::string, void(*)()>> tests;
+
+  if (func)
+    tests.push_back(std::make_pair(name, func));
+
+  return tests;
+}
+
+int main(int argc, char** argv)
+{
+  std::string color_pass = "\033[32m",
+              color_fail = "\033[31m",
+              color_warn = "\033[95m",
+              color_file = "\033[36m",
+              color_bold = "\033[1m",
+              color_dark = "\033[2m",
+              color_uline = "\033[4m",
+              color_reset = "\033[0m";
+  std::ostream* out = &std::cout;
+  bool fail_fast = false;
+
+  for (int i = 1; i < argc; i++)
+  {
+    std::string arg(argv[i]);
+
+    if (arg == "--no-color")
+    {
+      color_pass = color_fail = color_warn = color_file = color_bold =
+                   color_dark = color_uline = color_reset = "";
+    }
+    else if (arg == "--stderr")
+    {
+      out = &std::cerr;
+    }
+    else if (arg == "--fail-fast")
+    {
+      fail_fast = true;
+    }
+    else
+    {
+      std::cerr << "Unrecognized option: " << arg << std::endl;
+      return 2;
+    }
+  }
+
+  std::string warn = color_warn + color_bold + "warning" + color_reset + ": ";
+
+  auto& tests = g_tests();
+
+  auto start = std::chrono::steady_clock::now();
+  size_t passed = 0, failed = 0;
+
+  for (const auto& test : tests)
+  {
+    *out << "[  Running  ] " << color_bold << test.first << color_reset << "\r"
+         << std::flush;
+
+    auto test_start = std::chrono::steady_clock::now();
+
+    try
+    {
+      test.second();
+      passed++;
+
+      *out << color_pass << "[  SUCCESS  ] " << color_bold << test.first;
+    }
+    catch (const AssertFail& a)
+    {
+      failed++;
+
+      *out << color_fail << "[: FAILURE :] " << color_bold << test.first << ": "
+           << color_reset << color_file << a.get_path() << ": " << color_fail
+           << a.what();
+    }
+    catch (const std::exception& e)
+    {
+      failed++;
+
+      *out << color_fail << "[ ! ERROR ! ] " << color_bold << test.first
+           << color_reset << color_fail << ": " << e.what();
+    }
+    catch (const std::string& s)
+    {
+      failed++;
+
+      *out << color_fail << "[ ! ERROR ! ] " << color_bold << test.first
+           << color_reset << color_fail << ": " << s;
+    }
+    catch (const char* c)
+    {
+      failed++;
+
+      *out << color_fail << "[ ! ERROR ! ] " << color_bold << test.first
+           << color_reset << color_fail << ": " << c;
+    }
+    catch (...)
+    {
+      failed++;
+
+      *out << color_fail << "[ ! ERROR ! ] " << color_bold << test.first;
+    }
+
+    auto test_end = std::chrono::steady_clock::now();
+    float diff = static_cast<float>((test_end - test_start).count())
+                * static_cast<float>(decltype(test_end)::period::num)
+                / static_cast<float>(decltype(test_end)::period::den);
+
+    *out << color_reset << color_dark << " (" << str_time(diff) << ")\n"
+         << color_reset;
+
+    if (failed && fail_fast)
+      break;
+  }
+
+  auto end = std::chrono::steady_clock::now();
+  float diff = static_cast<float>((end - start).count())
+              * static_cast<float>(decltype(end)::period::num)
+              / static_cast<float>(decltype(end)::period::den);
+
+  *out << color_reset << "\n" << color_bold << color_uline
+       << (passed ? color_pass : "") << passed << color_reset  << " passed, "
+       << color_bold << color_uline << (failed ? color_fail : "") << failed
+       << color_reset << " failed." << color_dark << " (" << str_time(diff)
+       << ")" << color_reset << std::endl;
+
+  if (failed && fail_fast)
+    *out << "A test failed and --fail-fast was passed; some tests may not have "
+            "run." << std::endl;
+
+  return failed != 0;
+}
---
+++ tests/unit/util/vector.cpp
@@ -0,0 +1,275 @@
+//  SuperTux Meltdown - Semphris' take on the popular Linux platformer
+//  Copyright (C) 2022 Semphris <semphris@semphris.com>
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#include "tests.hpp"
+
+#include "util/vector.hpp"
+
+#include <cmath>
+
+#include "util/size.hpp"
+
+TEST(Vector__ctor)
+{
+  Vector v1;
+  EXPECT_FLT_EQ(v1.x, 0.0f);
+  EXPECT_FLT_EQ(v1.y, 0.0f);
+
+  Vector v2(1234.0f, -0.5f);
+  EXPECT_FLT_EQ(v2.x, 1234.0f);
+  EXPECT_FLT_EQ(v2.y, -0.5f);
+
+  Vector v3(Size(0.3125e-1f, -8.75e2f));
+  EXPECT_FLT_EQ(v3.x, 0.3125e-1f);
+  EXPECT_FLT_EQ(v3.y, -8.75e2f);
+}
+
+TEST(Vector__size)
+{
+  EXPECT_FLT_EQ(Vector().size().w, Size().w);
+  EXPECT_FLT_EQ(Vector().size().h, Size().h);
+  EXPECT_FLT_EQ(Vector(532.92f, 7537.765e-4f).size().w,
+                Size(532.92f, 7537.765e-4f).w);
+  EXPECT_FLT_EQ(Vector(532.92f, 7537.765e-4f).size().h,
+                Size(532.92f, 7537.765e-4f).h);
+}
+
+TEST(Vector__length)
+{
+  EXPECT_FLT_EQ(Vector().length(), 0.0f);
+  EXPECT_FLT_EQ(Vector(532.92f, 34.1f).length(), 534.0098654519f);
+}
+
+TEST(Vector__floor)
+{
+  EXPECT_FLT_EQ(Vector().floor().x, Vector().x);
+  EXPECT_FLT_EQ(Vector().floor().y, Vector().y);
+  EXPECT_FLT_EQ(Vector(5.1f, 3.9f).floor().x, Vector(5.0f, 3.0f).x);
+  EXPECT_FLT_EQ(Vector(5.1f, 3.9f).floor().y, Vector(5.0f, 3.0f).y);
+  EXPECT_FLT_EQ(Vector(-5.1f, -3.9f).floor().x, Vector(-6.0f, -4.0f).x);
+  EXPECT_FLT_EQ(Vector(-5.1f, -3.9f).floor().y, Vector(-6.0f, -4.0f).y);
+  EXPECT_FLT_EQ(Vector(-5.0f, 3.0f).floor().x, Vector(-5.0f, 3.0f).x);
+  EXPECT_FLT_EQ(Vector(-5.0f, 3.0f).floor().y, Vector(-5.0f, 3.0f).y);
+}
+
+TEST(Vector__round)
+{
+  EXPECT_FLT_EQ(Vector().round().x, Vector().x);
+  EXPECT_FLT_EQ(Vector().round().y, Vector().y);
+  EXPECT_FLT_EQ(Vector(5.1f, 3.9f).round().x, Vector(5.0f, 4.0f).x);
+  EXPECT_FLT_EQ(Vector(5.1f, 3.9f).round().y, Vector(5.0f, 4.0f).y);
+  EXPECT_FLT_EQ(Vector(5.5f, -5.5f).round().x, Vector(6.0f, -6.0f).x);
+  EXPECT_FLT_EQ(Vector(5.5f, -5.5f).round().y, Vector(6.0f, -6.0f).y);
+  EXPECT_FLT_EQ(Vector(-5.0f, 3.0f).floor().x, Vector(-5.0f, 3.0f).x);
+  EXPECT_FLT_EQ(Vector(-5.0f, 3.0f).floor().y, Vector(-5.0f, 3.0f).y);
+}
+
+TEST(Vector__ceil)
+{
+  EXPECT_FLT_EQ(Vector().ceil().x, Vector().x);
+  EXPECT_FLT_EQ(Vector().ceil().y, Vector().y);
+  EXPECT_FLT_EQ(Vector(5.1f, 3.9f).ceil().x, Vector(6.0f, 4.0f).x);
+  EXPECT_FLT_EQ(Vector(5.1f, 3.9f).ceil().y, Vector(6.0f, 4.0f).y);
+  EXPECT_FLT_EQ(Vector(-5.1f, -3.9f).ceil().x, Vector(-5.0f, -3.0f).x);
+  EXPECT_FLT_EQ(Vector(-5.1f, -3.9f).ceil().y, Vector(-5.0f, -3.0f).y);
+  EXPECT_FLT_EQ(Vector(-5.0f, 3.0f).ceil().x, Vector(-5.0f, 3.0f).x);
+  EXPECT_FLT_EQ(Vector(-5.0f, 3.0f).ceil().y, Vector(-5.0f, 3.0f).y);
+}
+
+TEST(Vector__operator_negate)
+{
+  Vector v1(0.0f, 0.0f);
+  EXPECT_FLT_EQ((-v1).x, -0.0f);
+  EXPECT_FLT_EQ((-v1).y, -0.0f);
+
+  Vector v2(1.0f, 1.0f);
+  EXPECT_FLT_EQ((-v2).x, -1.0f);
+  EXPECT_FLT_EQ((-v2).y, -1.0f);
+
+  Vector v3(-15643.65e7f, -1e-10f);
+  EXPECT_FLT_EQ((-v3).x, 15643.65e7f);
+  EXPECT_FLT_EQ((-v3).y, 1e-10f);
+}
+
+TEST(Vector__operator_plus)
+{
+  Vector v1(143.0f, -756.0f);
+  Vector v2(12.0f, 54.0f);
+
+  EXPECT_FLT_EQ((v1 + v2).x, 143.0f + 12.0f);
+  EXPECT_FLT_EQ((v1 + v2).y, -756.0f + 54.0f);
+}
+
+TEST(Vector__operator_plus_equals)
+{
+  Vector v1(143.0f, -756.0f);
+  v1 += Vector(12.0f, 54.0f);
+
+  EXPECT_FLT_EQ(v1.x, 143.0f + 12.0f);
+  EXPECT_FLT_EQ(v1.y, -756.0f + 54.0f);
+}
+
+TEST(Vector__operator_minus)
+{
+  Vector v1(143.0f, -756.0f);
+  Vector v2(12.0f, 54.0f);
+
+  EXPECT_FLT_EQ((v1 - v2).x, 143.0f - 12.0f);
+  EXPECT_FLT_EQ((v1 - v2).y, -756.0f - 54.0f);
+}
+
+TEST(Vector__operator_minus_equals)
+{
+  Vector v1(143.0f, -756.0f);
+  v1 -= Vector(12.0f, 54.0f);
+
+  EXPECT_FLT_EQ(v1.x, 143.0f - 12.0f);
+  EXPECT_FLT_EQ(v1.y, -756.0f - 54.0f);
+}
+
+TEST(Vector__operator_times__vector)
+{
+  Vector v1(143.0f, -756.0f);
+  Vector v2(12.0f, 54.0f);
+
+  EXPECT_FLT_EQ((v1 * v2).x, 143.0f * 12.0f);
+  EXPECT_FLT_EQ((v1 * v2).y, -756.0f * 54.0f);
+}
+
+TEST(Vector__operator_times__float)
+{
+  Vector v1(143.0f, -756.0f);
+
+  EXPECT_FLT_EQ((v1 * -27.0f).x, 143.0f * -27.0f);
+  EXPECT_FLT_EQ((v1 * -27.0f).y, -756.0f * -27.0f);
+}
+
+TEST(Vector__operator_times_equals__vector)
+{
+  Vector v1(143.0f, -756.0f);
+  v1 *= Vector(12.0f, 54.0f);
+
+  EXPECT_FLT_EQ(v1.x, 143.0f * 12.0f);
+  EXPECT_FLT_EQ(v1.y, -756.0f * 54.0f);
+}
+
+TEST(Vector__operator_times_equals__float)
+{
+  Vector v1(143.0f, -756.0f);
+  v1 *= -27.0f;
+
+  EXPECT_FLT_EQ(v1.x, 143.0f * -27.0f);
+  EXPECT_FLT_EQ(v1.y, -756.0f * -27.0f);
+}
+
+TEST(Vector__operator_divide__vector)
+{
+  Vector v1(143.0f, -756.0f);
+  Vector v2(12.0f, 54.0f);
+
+  EXPECT_FLT_EQ((v1 / v2).x, 143.0f / 12.0f);
+  EXPECT_FLT_EQ((v1 / v2).y, -756.0f / 54.0f);
+}
+
+TEST(Vector__operator_divide__float)
+{
+  Vector v1(143.0f, -756.0f);
+
+  EXPECT_FLT_EQ((v1 / -27.0f).x, 143.0f / -27.0f);
+  EXPECT_FLT_EQ((v1 / -27.0f).y, -756.0f / -27.0f);
+}
+
+TEST(Vector__operator_divide_equals__vector)
+{
+  Vector v1(143.0f, -756.0f);
+  v1 /= Vector(12.0f, 54.0f);
+
+  EXPECT_FLT_EQ(v1.x, 143.0f / 12.0f);
+  EXPECT_FLT_EQ(v1.y, -756.0f / 54.0f);
+}
+
+TEST(Vector__operator_divide_equals__float)
+{
+  Vector v1(143.0f, -756.0f);
+  v1 /= -27.0f;
+
+  EXPECT_FLT_EQ(v1.x, 143.0f / -27.0f);
+  EXPECT_FLT_EQ(v1.y, -756.0f / -27.0f);
+}
+
+TEST(Vector__operator_modulo__vector)
+{
+  Vector v1(143.0f, -756.0f);
+  Vector v2(12.0f, 54.0f);
+
+  EXPECT_FLT_EQ((v1 % v2).x, fmodf(143.0f, 12.0f));
+  EXPECT_FLT_EQ((v1 % v2).y, fmodf(-756.0f, 54.0f));
+}
+
+TEST(Vector__operator_modulo__float)
+{
+  Vector v1(143.0f, -756.0f);
+
+  EXPECT_FLT_EQ((v1 % -27.0f).x, fmodf(143.0f, -27.0f));
+  EXPECT_FLT_EQ((v1 % -27.0f).y, fmodf(-756.0f, -27.0f));
+}
+
+TEST(Vector__operator_modulo_equals__vector)
+{
+  // If anybody thinks this test suite is useless because the functions are
+  // "too simple" to be prone to mistakes, know that at the moment of writing
+  // this test, I had mistakenly written in Vector::operator%=() :
+  //
+  //     x = std::fmod(x, f);
+  //     y = std::fmod(x, f);
+  //     return *this;
+  //
+  // Notice the 'x' on the second line. Thanks to this test, I realised
+  // something was wrong, and I fixed the issue.
+
+  Vector v1(143.0f, -756.0f);
+  v1 %= Vector(12.0f, 54.0f);
+
+  EXPECT_FLT_EQ(v1.x, fmodf(143.0f, 12.0f));
+  EXPECT_FLT_EQ(v1.y, fmodf(-756.0f, 54.0f));
+}
+
+TEST(Vector__operator_modulo_equals__float)
+{
+  Vector v1(143.0f, -756.0f);
+  v1 %= -27.0f;
+
+  EXPECT_FLT_EQ(v1.x, fmodf(143.0f, -27.0f));
+  EXPECT_FLT_EQ(v1.y, fmodf(-756.0f, -27.0f));
+}
+
+TEST(Vector__operator_equals)
+{
+  EXPECT(Vector() == Vector());
+  EXPECT(Vector() == Vector(0.0f, 0.0f));
+  EXPECT(Vector(8764.0e7f, -1e10f) == Vector(8764.0e7f, -1e10f));
+  EXPECT(!(Vector(1.0f, 2.0f) == Vector(1.0f, 1.0f)));
+  EXPECT(!(Vector(2.0f, 1.0f) == Vector(1.0f, 1.0f)));
+}
+
+TEST(Vector__operator_not_equals)
+{
+  EXPECT(!(Vector() != Vector()));
+  EXPECT(!(Vector() != Vector(0.0f, 0.0f)));
+  EXPECT(!(Vector(8764.0e7f, -1e10f) != Vector(8764.0e7f, -1e10f)));
+  EXPECT((Vector(1.0f, 2.0f) != Vector(1.0f, 1.0f)));
+  EXPECT((Vector(2.0f, 1.0f) != Vector(1.0f, 1.0f)));
+}
