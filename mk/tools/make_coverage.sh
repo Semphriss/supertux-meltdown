ac1d0930003f2ebb8c3fa994893fe4cfcf12140f
Semphris <semphris@semphris.com>
2022-08-25 16:21:43 -0400

    Coverage analysis
    Adds a script that automatically generates an analysis of the coverage
    of the test suite.
    
    The coverage requires `lcov`, and its companion `genhtml`. On Ubuntu
    20.04, both come together as one package.

---
+++ mk/tools/make_coverage.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -e
+
+if ! command -v lcov &> /dev/null || ! command -v genhtml &> /dev/null; then
+  echo "lcov not found, not running script" >&2
+  exit 1
+fi
+
+cd "$(dirname "$0")"/../..
+
+mkdir -p build.cov
+rm -rf build.cov
+mkdir -p build.cov
+cd build.cov
+cmake -DCMAKE_CXX_FLAGS="-fprofile-arcs -ftest-coverage" -DCMAKE_EXE_LINKER_FLAGS="-lgcov --coverage" ..
+make -j11
+lcov --capture --initial --directory CMakeFiles/stmeltdown.dir/ --exclude '/usr/*' --exclude '*/src/main.cpp' --output-file coverage_initial.info
+./stmeltdown --test
+lcov --capture           --directory CMakeFiles/stmeltdown.dir/ --exclude '/usr/*' --exclude '*/src/main.cpp' --exclude 'CompilerIdCXX' --output-file coverage_test.info
+lcov --add-tracefile coverage_initial.info --add-tracefile coverage_test.info --output-file coverage_all.info
+lcov --remove coverage_all.info '*/tests/*' --output-file coverage.info
+mkdir -p coverage
+cd coverage
+genhtml ../coverage.info --demangle-cpp
